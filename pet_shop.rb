995b1158da8448d0697e33dfbcd8784aee9adf7b
Stephen <stephen_h369@live.co.uk>
2020-06-19 13:05:06 +0100

    test remove pet by name passes

--- pet_shop.rb
+++ pet_shop.rb
@@ -38,3 +38,11 @@ def find_pet_by_name(pet_shop, pet_name)
     end
     return nil
 end
+
+def remove_pet_by_name(pet_shop, pet_name)
+   for pet in pet_shop[:pets]
+        if (pet[:name] == pet_name)
+            pet_shop[:pets].delete(pet)
+        end
+    end
+end
