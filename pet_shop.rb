71baea3abe3ab9924022ca3c30a5000fcaca03db
Stephen <stephen_h369@live.co.uk>
2020-06-19 12:43:05 +0100

    test find pets by name returns pet passes

--- pet_shop.rb
+++ pet_shop.rb
@@ -31,3 +31,9 @@ def pets_by_breed(pet_shop, breed)
     end
     return pets
 end
+
+def find_pet_by_name(pet_shop, pet_name)
+    for pet in pet_shop[:pets]
+        return pet if (pet[:name] == pet_name)
+    end
+end
